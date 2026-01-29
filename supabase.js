c7384a7805b4e1edfd90d5a5883f85642c0f62d6
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2026-01-29 17:09:54 +0000

    fix

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://obywcpilionribalfrbl.supabase.co';
 
 // Правильный anon public key из Supabase Dashboard
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ieXdjcGlsaW9ucmliYWxmcmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MjQ5NTcsImV4cCI6MjA1MzMwMDk1N30.G3XgJRBHQB5rVSVVOUo0qsqVHXH2sC3c4PB9LVXgbzA';
+const SUPABASE_KEY = 'sb_publishable_BYToHeprZE-e64UjDgjlmQ_bKZBUFJ0';
 
 export const supabase = createClient(
   SUPABASE_URL,
