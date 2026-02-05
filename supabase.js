4253e560bd26d5c84114ac0626bccb4d9e685a2b
Alexis Kozlov <71926607+AlexisKozlov@users.noreply.github.com>
2026-02-05 05:00:55 +0000

    upd 83

--- supabase.js
+++ supabase.js
@@ -1,11 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = 'https://obywcpilionribalfrbl.supabase.co';
-
-
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ieXdjcGlsaW9ucmliYWxmcmJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNjQ4NzYsImV4cCI6MjA4NDc0MDg3Nn0.gOo7_prJfR7kxw6Fe8ZrEEIbhEK02DCNYPr9Ln2EuYc';
 
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
